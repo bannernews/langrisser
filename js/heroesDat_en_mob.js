356c46d7cc68d363d459f5544ed766747f7c260a
bannernews <143966805+bannernews@users.noreply.github.com>
2024-10-01 14:36:16 +0300

    Add files via upload

---
+++ js/heroesDat_en_mob.js
@@ -0,0 +1,24 @@
+// soldDat.length  -  колл-во всех солдат
+// soldDat[0].length  -  колл-во элементов у солдат
+// 0 - имя героя, 1 - англ имя героя, 2 - фракция, 3 - ранг, 4 - ковка, 5 - СП, 6 - часть
+
+function heroesView () {
+	var sd = document.getElementById('heroes_data_mob');
+//	sd.innerHTML = '<table><tr><th>Имя героя</th><th>Бонд для ОЗ,ЗАЩ</th><th>Бонд для АТК/ИНТ</th><th>Нужен для бондов другим героям</th></tr>';
+	sd.innerHTML = "";
+	//sd.innerHTML = soldDat[111][8];
+	var i;
+	for (i=1;i<heroesDat.length;i++){
+		fact = heroesDat[i][2].split(",");
+		if (fact[0]!=""){
+				f2 = "her";
+				for (j=0;j<fact.length;j++){
+					f2 += ' ' + fact[j];
+				}
+			} else {f2 = "her";}
+		hreff = ('hero_en_mob.html?name='+ heroesDat[i][0]).replace(/ /g,"%20");
+		if(heroesDat[i][0]=='Трансцендентный' || heroesDat[i][0]=='Повелительница ледяной бездны'){f = 'font-size:8px;'}else{f = 'font-size:9px;'}
+		sd.innerHTML += '<div description="' + heroesDat[i][0] + '" class="' + f2 + ' ' + heroesDat[i][3] + ' ' + heroesDat[i][4] + ' ' + heroesDat[i][5] + ' ' + heroesDat[i][6] + '" style="display:block;height:80px;width:69px;float:left;"><div class="imgglow" style="display:block;"><a href="'+hreff+'"><img style="cursor:pointer;" height="60px" src="images/head/' + heroesDat[i][0] + '.png" title="' + heroesDat[i][1] + '"></a></div><div style="white-space:normal;' + f + 'width:60px;text-align:center;margin-top:-5px;">' + heroesDat[i][1] + '</div></div>';
+//		}
+	}
+}
