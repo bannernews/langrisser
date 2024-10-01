1ffb623df8f86687c605d5f5c25ef6ec6507b549
bannernews <143966805+bannernews@users.noreply.github.com>
2024-10-01 14:22:11 +0300

    Add files via upload

--- js/main_mob.js
+++ js/main_mob.js
@@ -22,8 +22,8 @@ for (var i2 = 0; i2 < dis.length; i2++) {
 var iC = filterCont.getElementsByClassName("filt_i");
 for (var i2 = 0; i2 < iC.length; i2++) {
   iC[i2].addEventListener("click", function(){
-    var current2 = document.getElementsByClassName("active_item");
-		if (current2[0]){current2[0].className = current2[0].className.replace(" active_item", "")};
+    var current2 = document.getElementsByClassName("active_h");
+		if (current2[0]){current2[0].className = current2[0].className.replace(" active_h", "")};
 		if (this.classList.contains('weapony')){
 			document.getElementById("weaponi").src="images/itemIcons/weapon_active.png";
 			document.getElementById("bodyi").src="images/item_type/body.png";
@@ -86,6 +86,49 @@ for (var i = 0; i < filt_h.length; i++) {
 		}
 		else{
 			for (h=0;h<hers.length;h++){hers[h].style.display = 'block'}
+		}
+  });
+}
+
+var it_type = document.querySelectorAll('.it_type'); // кнопки - фильтры
+var itd = document.getElementById('item_data'); // иконки героев
+var ss='';
+for (var i = 0; i < it_type.length; i++) {
+	it_type[i].chosen = false; // Изначально все показаны
+	it_type[i].addEventListener('click', function() {
+//		var current2i = document.getElementsByClassName("active_i");
+//		for (var i3 = 0; i3 < current2i.length; i3++) {
+//			if (current2i[i3].classList.contains('active_i')){
+//				current2i[i3].className = current2i[i3].className.replace(" active_i", "")
+//			}
+//		}
+//		if (current2i[0]){current2i[0].className = current2i[0].className.replace(" active_i", "")};
+		if (this.classList.contains('active_i')){this.className = this.className.replace(" active_i", "")}else{
+		this.className += " active_i"}
+		
+//		console.log(this.title);
+		this.chosen = !this.chosen; // Значение переключается, как у обычных чекбоксов
+		if(ss.indexOf('.')==0){
+			reps = '.' + this.title;
+//			ff +=',';
+		}else{
+			reps = '.' + this.title;
+		}
+		
+		if(!this.chosen){ss=ss.replace(reps, '')} else {ss += '.' + this.title;}
+		this.classList.toggle('checked');
+		
+		console.log(ss);
+		var itds = itd.querySelectorAll('.all'); 
+		for (h=0;h<itds.length;h++){itds[h].style.display = 'none'}
+		if (ss!==''){
+			var item = itd.querySelectorAll(ss); 
+			for( var u = 0; u < item.length; u++ ){
+				item[u].style.display = 'block';
+			}
+		}
+		else{
+			for (h=0;h<itds.length;h++){itds[h].style.display = 'block'}
 		}
   });
 }
