b144d0cfceabc0870d2d21deb433ef1c662e0a3a
bannernews <143966805+bannernews@users.noreply.github.com>
2025-05-16 23:36:18 +0300

    Update dataTable_en.js

--- js/dataTable_en.js
+++ js/dataTable_en.js
@@ -1114,10 +1114,10 @@ function bestBuildOpen(){
 var fetter_pic=document.getElementById("fetter_pic");
 fetter_title=document.getElementById("fetter_title");
 var imgFetter = new Image();
-imgFetter.src = "images/heroes/heroes_list/"+ heroName +"/Confession/Confession.png";
+imgFetter.src = "images/heroes/heroes_list/"+ heroName +"/Confession/Confession.jpg";
 imgFetter.onload = function(){
-	fetter_pic.innerHTML = '<img src="images/heroes/heroes_list/'+ heroName +'/Confession/Confession.png" width="1320px" />';
-	fetter_title.innerHTML += '<span style="line-height:2;display:block;width:120px;margin:0 auto;height:35px;">Confession  <img class="conf_hover" style="position:absolute;vertical-align:middle;cursor:pointer;" src="images/icon_confession.png" height="30px" onclick=window.open("'+confession[hero_number][1]+'","new_window"); /></span>';
+	fetter_pic.innerHTML = '<img src="images/heroes/heroes_list/'+ heroName +'/Confession/Confession.jpg" width="1320px" />';
+	fetter_title.innerHTML += '<span style="line-height:2;display:block;width:120px;margin:0 auto;height:35px;">Confession  <img class="conf_hover" style="position:absolute;vertical-align:middle;cursor:pointer;" src="images/icon_confession.jpg" height="30px" onclick=window.open("'+confession[hero_number][1]+'","new_window"); /></span>';
 };
 imgFetter.onerror = function(){
 	fetter_pic.style.display='none';
